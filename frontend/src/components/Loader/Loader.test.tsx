651abcd84615d518e563ba2d9596b3542dc652ad
Manuel-M-M <mmoragamolina@gmail.com>
2025-02-18 18:18:56 +0100

    test(frontend): add unit tests for Loader component following TDD and clean architecture

---
+++ frontend/src/components/Loader/Loader.test.tsx
@@ -0,0 +1,20 @@
+import '@testing-library/jest-dom';
+import 'jest-styled-components';
+import { render, screen } from '@testing-library/react';
+import { Loader } from './Loader';
+
+describe('Loader Component', () => {
+  it('should render correctly', () => {
+    render(<Loader />);
+    expect(screen.getByLabelText(/loading/i)).toBeInTheDocument();
+  });
+
+  it('should have a spinning animation', () => {
+    render(<Loader />);
+    const loader = screen.getByLabelText(/loading/i);
+    expect(loader).toHaveStyleRule(
+      'animation',
+      expect.stringMatching(/infinite/),
+    );
+  });
+});
